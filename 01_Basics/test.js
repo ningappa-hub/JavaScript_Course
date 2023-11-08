6f1c4758e6c81837cf5bdb641bc6cf1d06bffb3f
Ningappa <72871139+ningappa-hub@users.noreply.github.com>
2023-11-08 08:52:26 +0000

    Variables var, let, const

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,7 @@
-console.log("Ningappa");
+const accountId = 14452
+let accountEmail = "ningappa@google.com"
+var accountPassword = "12345"
+accountCity = "Bengalur"
+let accountState
+
+console.log(accountId, accountEmail, accountPassword, accountCity, accountState);
