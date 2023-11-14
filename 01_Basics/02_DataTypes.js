14d41ef92ad24b130686ee1e6d695d9991ba597f
Ningappa <72871139+ningappa-hub@users.noreply.github.com>
2023-11-14 07:54:27 +0000

    Data types

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3); console.log("Ningappa");
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
